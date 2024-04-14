b4f93ae7980eb8ec36dcce63c47d1c04fa83ea38
Santy2304 <a20210751@pucp.edu.pe>
2024-04-13 21:28:53 -0500

    hola

---
+++ src/main/java/com/example/proyectogrupo4_gtics/ProyectoGrupo4GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectogrupo4_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo4GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoGrupo4GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectogrupo4_gtics/ProyectoGrupo4GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectogrupo4_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo4GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

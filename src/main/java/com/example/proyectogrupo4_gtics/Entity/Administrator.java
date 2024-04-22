94cfd8a29a51fc60614023f36dad0f366de70be1
J3anP <a20213733@pucp.edu.pe>
2024-04-22 18:31:55 -0500

    Doctor,Administrator y Superadmin

--- src/main/java/com/example/proyectogrupo4_gtics/Entity/Administrator.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Entity/Administrator.java
@@ -1,4 +1,38 @@
 package com.example.proyectogrupo4_gtics.Entity;
+import com.mysql.cj.jdbc.Blob;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Administrator {
+@Getter
+@Setter
+@Entity
+@Table(name="administrator")
+public class Administrator{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="idadministrador")
+    private int idAdministrador;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="lastname")
+    private String lastName;
+
+    @Column(name="dni")
+    private String dni;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="banned")
+    private Boolean banned;
+
+    @Column(name="photo")
+    private String photo; /*Persistence no me deja poner Blob*/
+
 }

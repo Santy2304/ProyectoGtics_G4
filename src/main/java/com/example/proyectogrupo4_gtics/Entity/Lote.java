4e560796d27595c9ece96ccae155efd169a70928
Santy2304 <a20210751@pucp.edu.pe>
2024-04-23 12:34:16 -0500

    entidades de medicamentos y lotes

--- src/main/java/com/example/proyectogrupo4_gtics/Entity/Lote.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Entity/Lote.java
@@ -1,4 +1,64 @@
 package com.example.proyectogrupo4_gtics.Entity;
 
-public class Lote {
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+import java.util.Date;
+
+public class Lote{
+
+
+    @Getter
+    @Id
+    @Column(name = "idlote")
+    private int idLote;
+
+    @Getter
+    private String site;
+
+    @Getter
+    @Column(name = "expiredate")
+    private Date expireDate;
+
+    @Getter
+    @Column(name = "initialstock")
+    private int initialStock;
+
+    @Getter
+    private boolean expire;
+
+    @Getter
+    private int stock;
+
+    @ManyToOne
+    @JoinColumn(name="idpedidosreposicion")
+    private ReplacementOrder replacementOrder;
+
+    public void setIdLote(int idLote) {
+        this.idLote = idLote;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public void setExpireDate(Date expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public void setInitialStock(int initialStock) {
+        this.initialStock = initialStock;
+    }
+
+    public void setExpire(boolean expire) {
+        this.expire = expire;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
 }
--- src/main/java/com/example/proyectogrupo4_gtics/Repository/MedicineRepository.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/MedicineRepository.java
@@ -1,4 +1,8 @@
 package com.example.proyectogrupo4_gtics.Repository;
 
-public interface MedicineRepository {
+import com.example.proyectogrupo4_gtics.Entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Medicine,Integer> {
+
 }

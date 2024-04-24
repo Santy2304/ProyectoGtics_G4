9d0bfdd9ecce7b31f88c6820660cf3a12a6b1ce2
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-24 17:27:38 -0500

    fix:Superadmin vistas

--- src/main/java/com/example/proyectogrupo4_gtics/Controller/SuperAdminController.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Controller/SuperAdminController.java
@@ -23,13 +23,71 @@ public class SuperAdminController {
         this.doctorRepository = doctorRepository;
     }
 
-    @GetMapping("/listaMedicamentos")
-    public String listar(Model model) {
+    @GetMapping("/listaMedicamentosSuperAdmin")
+    public String listarMedicamentos(Model model) {
 
         model.addAttribute("listaMedicamentos", medicineRepository.obtenerDatosMedicamentos());
 
         return "superAdmin/listaMedicamentos";
     }
+    //Solo para poder saltar entre vistas auxiliar de momento
+    @GetMapping("/verSedeSuperAdmin")
+    public String verSede(Model model) {
+        return "superAdmin/Sede";
+    }
+    @GetMapping("/verUserListSuperAdmin")
+    public String verUserList() {
+        return "superAdmin/userlist";
+    }
+    @GetMapping("/verPerfilSuperAdmin")
+    public String verPerfil() {
+        return "superAdmin/perfil";
+    }
+    @GetMapping("/verNotificationsSuperAdmin")
+    public String verNotifications() {
+        return "superAdmin/notifications";
+    }
+    @GetMapping("/verListadosSuperAdmin")
+    public String verListados() {
+        return "superAdmin/listados";
+    }
+    @GetMapping("/verEditarMedicamentoSuperAdmin")
+    public String verEditarMedicamento() {
+        return "superAdmin/editarMedicamento";
+    }
+    @GetMapping("/verEditarFarmacistaSuperAdmin")
+    public String verEditarFarmacista() {
+        return "superAdmin/EditarFarmacista";
+    }
+    @GetMapping("/verEditarDoctorSuperAdmin")
+    public String verEditarDoctor() {
+        return "superAdmin/EditarDoctor";
+    }
+    @GetMapping("/verEditarAdministradorSuperAdmin")
+    public String verEditarAdministrador() {
+        return "superAdmin/EditarAdministrador";
+    }
+    @GetMapping("/verDetallesProductoSuperAdmin")
+    public String verDetallesProducto() {
+        return "superAdmin/detallesProducto";
+    }
+    @GetMapping("/verDetalleMedicamentosSuperAdmin")
+    public String verDetalleMedicamentos() {
+        return "superAdmin/DetalleMedicamentos";
+    }
+    @GetMapping("/verAñadirMedicamentoSuperAdmin")
+    public String verAddMedicamento() {
+        return "superAdmin/añadirMedicamento";
+    }
+    @GetMapping("/verAgregarDoctorSuperAdmin")
+    public String verAgregarDoctor() {
+        return "superAdmin/AgregarDoctor";
+    }
+    @GetMapping("/verAgregarAdminSedeSuperAdmin")
+    public String verAgregarAdminSede() {
+        return "superAdmin/AgregarAdminSede";
+    }
+
 
 
 

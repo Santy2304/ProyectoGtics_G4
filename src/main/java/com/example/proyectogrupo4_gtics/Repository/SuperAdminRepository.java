00f94c96c423b50f111a27b80ee3221a617a8efc
J3anP <a20213733@pucp.edu.pe>
2024-04-24 01:32:29 -0500

    Lista medicinas y lista medicinas por nombre

--- src/main/java/com/example/proyectogrupo4_gtics/Repository/SuperAdminRepository.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/SuperAdminRepository.java
@@ -1,9 +1,6 @@
 package com.example.proyectogrupo4_gtics.Repository;
 
-import com.example.proyectogrupo4_gtics.Entity.Administrator;
-import com.example.proyectogrupo4_gtics.Entity.Doctor;
-import com.example.proyectogrupo4_gtics.Entity.Patient;
-import com.example.proyectogrupo4_gtics.Entity.SuperAdmin;
+import com.example.proyectogrupo4_gtics.Entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -21,6 +18,7 @@ public interface SuperAdminRepository extends JpaRepository<SuperAdmin, Integer>
 
     //Listar farmacista
     //@Query(value = "select * from pharmacist", nativeQuery = true)
+    //List<Pharmacist> listaFarmacista();
 
     //Listar paciente
     @Query(value = "select * from patient", nativeQuery = true)
@@ -38,6 +36,14 @@ public interface SuperAdminRepository extends JpaRepository<SuperAdmin, Integer>
     //Buscar pacientes por nombre
     @Query(value="select * from patients where name=?", nativeQuery = true)
     List<Patient> listarPacientePorNombre(String name);
+
+    //Listar medicamentos
+    @Query(value="select * from medicine", nativeQuery = true)
+    List<Medicine> listaMedicamentos();
+
+    //Listar medicamento por nombre
+    @Query(value ="select * from medicine where name = ?", nativeQuery = true)
+    List<Medicine> listarMedicamentoPorNombre(String name);
 
 
 

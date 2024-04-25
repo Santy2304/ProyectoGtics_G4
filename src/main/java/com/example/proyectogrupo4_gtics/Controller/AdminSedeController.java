4d04dd71c1494c54dc03428c469e189bf62f4b95
J3anP <a20213733@pucp.edu.pe>
2024-04-25 12:07:10 -0500

    Aiudaa

--- src/main/java/com/example/proyectogrupo4_gtics/Controller/AdminSedeController.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Controller/AdminSedeController.java
@@ -1,4 +1,25 @@
 package com.example.proyectogrupo4_gtics.Controller;
 
+import com.example.proyectogrupo4_gtics.Entity.Doctor;
+import com.example.proyectogrupo4_gtics.Repository.DoctorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AdminSedeController {
+    final DoctorRepository doctorRepository;
+
+    public AdminSedeController(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+
+    @GetMapping("/listaDoctoresAdminSede")
+    public String listDoctors(Model model) {
+        model.addAttribute("listaDoctores", doctorRepository.listaDoctorPorSede());
+        return "admin_sede/doctorlist";
+    }
+
 }

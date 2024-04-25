d8930881f5853c5ee14eeb459c23d7cd2f202ffb
J3anP <a20213733@pucp.edu.pe>
2024-04-25 16:42:15 -0500

    admin sede arreglado

--- src/main/java/com/example/proyectogrupo4_gtics/Controller/AdminSedeController.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Controller/AdminSedeController.java
@@ -1,7 +1,10 @@
 package com.example.proyectogrupo4_gtics.Controller;
 
 import com.example.proyectogrupo4_gtics.Entity.Doctor;
+import com.example.proyectogrupo4_gtics.Entity.Medicine;
 import com.example.proyectogrupo4_gtics.Repository.DoctorRepository;
+import com.example.proyectogrupo4_gtics.Repository.MedicineRepository;
+import com.example.proyectogrupo4_gtics.Repository.PharmacistRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class AdminSedeController {
     final DoctorRepository doctorRepository;
-
-    public AdminSedeController(DoctorRepository doctorRepository) {
+    final PharmacistRepository pharmacistRepository;
+    final MedicineRepository medicineRepository;
+    public AdminSedeController(DoctorRepository doctorRepository, PharmacistRepository pharmacistRepository, MedicineRepository medicineRepository) {
         this.doctorRepository = doctorRepository;
+        this.pharmacistRepository = pharmacistRepository;
+        this.medicineRepository = medicineRepository;
     }
 
 
@@ -21,5 +27,42 @@ public class AdminSedeController {
         model.addAttribute("listaDoctores", doctorRepository.listaDoctorPorSede());
         return "admin_sede/doctorlist";
     }
+    @GetMapping("listaFarmacistaAdminSede")
+    public String listPharmacist(Model model) {
+        model.addAttribute("listaFarmacista", pharmacistRepository.listaFarmacistaPorSede());
+        return "admin_sede/pharmacistlist";
+    }
+    /*Linkear las demás vistas*/
+    @GetMapping("dashboardAdminSede")
+    public String verDashboard(Model model) {
+        return "admin_sede/Dashboard";
+    }
 
+    @GetMapping("inventarioAdminSede")
+    public String verInventario(Model model) {
+        return "admin_sede/inventario";
+    }
+
+    @GetMapping("verAddPharmacist")
+    public String verAddPharmacist(Model model) {
+        return "admin_sede/addpharmacist";
+    }
+
+    @GetMapping("verListaReposicion")
+    public String listaReposicion(Model model) {
+        return "admin_sede/listaReposicion";
+    }
+
+    @GetMapping("/verNotificaciones")
+    public String notificaciones(Model model) {
+        return "admin_sede/notifications";
+    }
+    @GetMapping("/verPerfil")
+    public String profile(Model model){
+        return "admin_sede/profile";
+    }
+    @GetMapping("/verSolicitudReposicion")
+    public String solicitudReposicion(Model model){
+        return "admin_sede/reposicion";
+    }
 }
--- src/main/java/com/example/proyectogrupo4_gtics/Repository/PharmacistRepository.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/PharmacistRepository.java
@@ -3,7 +3,11 @@ import com.example.proyectogrupo4_gtics.Entity.Pharmacist;
 
 import com.example.proyectogrupo4_gtics.Entity.Pharmacist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PharmacistRepository extends JpaRepository<Pharmacist, Integer> {
-
-}
+    @Query(nativeQuery = true,value="select p.idPharmacist as idPharmacist, p.name as name,p.lastName as apellido,p.site as sede,p.dni as dni,p.code as codigo,p.email as email,p.photo as foto from pharmacist p inner join administrator a where a.site=p.site")
+    List<FarmacistaPorSedeDTO> listaFarmacistaPorSede();
+ }

b882254f892bf556a07544d5e41502715168d531
J3anP <a20213733@pucp.edu.pe>
2024-04-25 13:00:51 -0500

    Pharmacist

---
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/PharmacistRepository.java
@@ -0,0 +1,7 @@
+package com.example.proyectogrupo4_gtics.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacistRepository extends JpaRepository<Pharmacist, Integer> {
+
+}

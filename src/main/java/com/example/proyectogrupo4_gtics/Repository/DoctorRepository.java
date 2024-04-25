b48798442b8ddb0a4ba4e6365978c568d491ae0e
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-25 14:36:38 -0500

    vistas arregladas :D

--- src/main/java/com/example/proyectogrupo4_gtics/Repository/DoctorRepository.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/DoctorRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
-    @Query(value = "select d.idDoctor as idDoctor, d.name as nombre, d.lastName as lastName, d.dni as dni, d.headquarter as sede, d.email as email from doctor d inner join administrator a where (a.site=d.headquarter)")
+    @Query(nativeQuery = true, value = "select d.idDoctor as idDoctor, d.name as nombre, d.lastName as lastName, d.dni as dni, d.headquarter as sede, d.email as email from doctor d inner join administrator a where (a.site=d.headquarter)")
     List<DoctorPorSedeDTO> listaDoctorPorSede();
 }
--- src/main/java/com/example/proyectogrupo4_gtics/Repository/PharmacistRepository.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Repository/PharmacistRepository.java
@@ -1,5 +1,6 @@
 package com.example.proyectogrupo4_gtics.Repository;
 
+import com.example.proyectogrupo4_gtics.Entity.Pharmacist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PharmacistRepository extends JpaRepository<Pharmacist, Integer> {

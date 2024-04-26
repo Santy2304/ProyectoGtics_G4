2ed665ad54caf524d4ddd95956c3a7b19790dfbc
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-26 11:37:51 -0500

    Creando Controllers para vistas de farmacista

--- src/main/java/com/example/proyectogrupo4_gtics/Controller/PharmacistController.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Controller/PharmacistController.java
@@ -1,4 +1,49 @@
 package com.example.proyectogrupo4_gtics.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class PharmacistController {
+    @GetMapping("/verChatFarmacista")
+    public String verChatPharmacist(){
+        return "pharmacist/chat";
+    }
+
+    @GetMapping("/verEditarProductoFarmacista")
+    public String verEditProduct(){
+        return "pharmacist/editproduct";
+    }
+
+    @GetMapping("/verMedicinelistFarmacista")
+    public String verMedicineList(){
+        return "pharmacist/medicinelist";
+    }
+
+    @GetMapping("/verNotificationsFarmacista")
+    public String verNotifications(){
+        return "pharmacist/notifications";
+    }
+
+    @GetMapping("/posFarmacista")
+    public String verPosPharmacist(){
+        return "pharmacist/pos";
+    }
+
+    @GetMapping("/productDetails")
+    public String verProductDetails(){
+        return "pharmacist/product-details";
+    }
+
+    @GetMapping("/verProductListFarmacista")
+    public String verProductList(){
+        return "pharmacist/productlist";
+    }
+
+    @GetMapping("/verProfileFarmacista")
+    public String verPerfilPharmacist(){
+        return "pharmacist/profile";
+    }
+
 }

5554f38799328d8948c39284dd2dcd79a115d96f
Hineill <hineill.cespedes@pucp.edu.pe>
2024-06-28 13:12:17 -0500

    Holaaaa

--- src/main/java/com/example/proyectogrupo4_gtics/Controller/PatientController.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Controller/PatientController.java
@@ -256,7 +256,7 @@ public class PatientController {
             //ruta relativa para la imagen
             //Path directorioImagenMedicine= Paths.get("src//main//resources//static//assets_superAdmin//ImagenesMedicina");
             //String rutaAbsoluta =  directorioImagenMedicine.toFile().getAbsolutePath();
-            String rutaAbsoluta = "C://SaintMedic//imagenes";
+            String rutaAbsoluta = "//SaintMedic//imagenes";
             //imagen a flujo bytes y poder guardarlo en la base de datos para poder extraerlo después
             try {
                 byte[] bytesImgMedicine = receta.getBytes();

719af0722e52b5fadda3c2552ade28eb0191cba4
Hineill <hineill.cespedes@pucp.edu.pe>
2024-11-04 16:25:08 -0500

    Holaa

--- src/main/java/com/example/proyectogrupo4_gtics/Service/Dialogflow.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Service/Dialogflow.java
@@ -26,7 +26,7 @@ public class Dialogflow {
         this.resourceLoader = resourceLoader;
     }
     public String detectIntent(String text, String sessionId) throws IOException {
-            Resource resource = resourceLoader.getResource("classpath:bot-nuevo-440622-ef0e6ccb90d9.json");
+            Resource resource = resourceLoader.getResource("classpath:bot-nuevo-440622-d782e4fb7e98.json");
             GoogleCredentials credentials;
             try (InputStream credentialsStream = resource.getInputStream()) {
                 credentials = GoogleCredentials.fromStream(credentialsStream)

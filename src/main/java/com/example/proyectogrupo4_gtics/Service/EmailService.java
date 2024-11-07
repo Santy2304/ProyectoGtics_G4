433f36b884475e6b588d9348b07faa2c1d465762
Hineill <hineill.cespedes@pucp.edu.pe>
2024-11-06 20:37:01 -0500

    Holaaaa

--- src/main/java/com/example/proyectogrupo4_gtics/Service/EmailService.java
+++ src/main/java/com/example/proyectogrupo4_gtics/Service/EmailService.java
@@ -17,67 +17,51 @@ import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
-
     @Autowired
     private JavaMailSender emailSender;
-
     public void sendEmailWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
-
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(text);
-
         FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
         helper.addAttachment("Image", file);
-
         emailSender.send(message);
     }
-
-
     public void sendHtmlForgetPassword(String to, String subject , String token) throws MessagingException, IOException {
-
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         // Carga de la plantilla HTML desde los recursos
         var resource = new ClassPathResource("emailForgotPassword.html");
         String html = new String(StreamUtils.copyToByteArray(resource.getInputStream()), StandardCharsets.UTF_8);
-        // Reemplazar marcadores de posición en la plantilla
         helper.setFrom("syong7350@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
+        String ola=  "https://saintmedic.online/changePassword?tk="+token;
+        html = html.replace("{{ola}}" , token );
         helper.setText(html, true); // true indica que el mensaje es HTML
-        html.replace("{{token}}" , token );
         ClassPathResource bannerResource = new ClassPathResource("static/assets_superAdmin/img/banner.png");
         helper.addInline("bannerImage", bannerResource);
         emailSender.send(message);
     }
-
-
     public void sendHtmlMessage(String to, String subject, String name, String password) throws MessagingException, IOException {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
         // Carga de la plantilla HTML desde los recursos
         var resource = new ClassPathResource("email.html");
         String html = new String(StreamUtils.copyToByteArray(resource.getInputStream()), StandardCharsets.UTF_8);
-
         // Reemplazar marcadores de posición en la plantilla
         html = html.replace("{{name}}", name);
         html = html.replace("{{password}}", password);
-
         helper.setFrom("syong7350@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(html, true); // true indica que el mensaje es HTML
-
         ClassPathResource bannerResource = new ClassPathResource("static/assets_superAdmin/img/banner.png");
         helper.addInline("bannerImage", bannerResource);
-
         emailSender.send(message);
     }
-
 
     public void sendHtmlRechazo(String to, String subject, String name,String motivo) throws MessagingException, IOException {
         MimeMessage message = emailSender.createMimeMessage();
